563bf48d5e97f41871b388403a5085316c0f52a0
Vladimir Plokhotnichenko <vladimir_plokhotnichenko@protonmail.com>
2025-03-28 22:00:36 +0300

    removed comments from script.js (it handles collapsibles in some pages)

--- scp/articles_html/script.js
+++ scp/articles_html/script.js
@@ -1,19 +1,4 @@
 function loadscript() {
-  /*
-  foldedElement = document.querySelector('.collapsible-block-folded')
-  unfoldedElement = document.querySelector('.collapsible-block-unfolded')
-  foldedLink = foldedElement.querySelector('.collapsible-block-link')
-  unfoldedLinks = unfoldedElement.querySelectorAll(':scope > .collapsible-block-unfolded-link > .collapsible-block-link')
-  foldedLink.addEventListener('click', () => {
-    unfoldedElement.style.display = 'block'
-    foldedElement.style.display = 'none'
-  })
-  unfoldedLinks.forEach((unfoldedLink) => unfoldedLink.addEventListener('click', () => {
-    unfoldedElement.style.display = 'none'
-    foldedElement.style.display = 'block'
-  }))
-  */
-
   function createFolder(ue, fe) {
     return function () {
       ue.style.display = 'block'
@@ -34,19 +19,7 @@ function loadscript() {
     unfoldedElement = cb.querySelector('.collapsible-block-unfolded')
     foldedLink = foldedElement.querySelector('.collapsible-block-link')
     unfoldedLinks = unfoldedElement.querySelectorAll('.collapsible-block-unfolded-link')
-    // foldedLink.addEventListener('click', () => {
-      // console.log('clicked', foldedLink.innerHTML)
-      // unfoldedElement.style.display = 'block'
-      // foldedElement.style.display = 'none'
-
-    // })
     foldedLink.addEventListener('click', createFolder(unfoldedElement,foldedElement))
-    // unfoldedLinks.forEach((unfoldedLink) => unfoldedLink.addEventListener('click', () => {
-      // console.log('clicked', unfoldedLink.innerHTML)
-      // unfoldedElement.style.display = 'none'
-      // foldedElement.style.display = 'block'
-    // }))
     unfoldedLinks.forEach((unfoldedLink) => unfoldedLink.addEventListener('click', createUnfolder(unfoldedElement,foldedElement)))
   })
-
 }
