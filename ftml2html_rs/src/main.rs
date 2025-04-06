4b15ae87f97b2384d68ab390292123ded3dd848f
Vladimir Plokhotnichenko <vladimir_plokhotnichenko@protonmail.com>
2025-04-06 08:05:01 +0300

    fixed charset issue while using kiwix desktop (should've set charset from the start)

--- ftml2html_rs/src/main.rs
+++ ftml2html_rs/src/main.rs
@@ -51,10 +51,10 @@ fn main() {
     let (tree, _warnings) = result.into();
     let html_output = HtmlRender.render(&tree, &page_info, callbacks, &settings);
 
-    let head = "<script src='script.js'></script><link rel='stylesheet' href='style.css'>";
+    let head = "<meta charset='utf-8'><script src='script.js'></script><link rel='stylesheet' href='style.css'>";
 
     // TODO - maybe add tags between title and html_output.body
-    let mut content = "<html><title>".to_owned() + &title + "</title>"+head+ "</head><body onload='loadscript();'>" +"<h1>" + &title + "</h1>" + &html_output.body + "</body></html>";
+    let mut content = "<html><head><title>".to_owned() + &title + "</title>"+head+"</head><body onload='loadscript();'>" +"<h1>" + &title + "</h1>" + &html_output.body + "</body></html>";
     content = content.replace("\\n","<br>");
     let re = Regex::new(r#"href="/(.*?)""#).unwrap();
     content = re.replace_all(&content, r#"href="$1.html""#).to_string();
